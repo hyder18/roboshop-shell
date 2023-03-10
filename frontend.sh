f101ecfb29f22e6ab03cb39320d3240eae102dfa
shammi <shammijb123@gmail.com>
2023-03-10 16:11:13 +0530

    frontend service

--- frontend.sh
+++ frontend.sh
@@ -1,4 +1,5 @@
 code_dir=$[pwd]
+
 echo -e "\e[33minstalling nginx\e[0m"
 yum install nginx -y
 
@@ -12,7 +13,7 @@ echo-e "\e[extracting downloaded frontend\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-eecho -e "\e[35mcopying nginx config for roboshop\e[0m"
+echo -e "\e[35mcopying nginx config for roboshop\e[0m"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx.deafult.d/roboshop.conf
 
 echo -e "\e[35menable nginx\e[0m"
