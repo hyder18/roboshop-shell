7b0dd494ac8daf63f7926ccd0352faac27c8e8f9
hyder18 <Shammijb123@gmail.com>
2023-06-19 16:45:20 +0530

    roboshop frontend

--- frontend.sh
+++ frontend.sh
@@ -13,7 +13,7 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
 echo -e "\e[35mcopying nginx config for roboshop\e[0m"
-cp ${code_dir}/configs/nginx-roboshop /etc/nginx/deafult.d/roboshop.conf
+cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/deafult.d/roboshop.conf
 
 echo -e "\e[35menabling nginx\e[0m"
 systemctl enable nginx
