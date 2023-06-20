63854761baa0328d68fa331f6a9e785e32cecadc
hyder18 <Shammijb123@gmail.com>
2023-06-20 16:26:03 +0530

    roboshop-component

--- catalogue.sh
+++ catalogue.sh
@@ -18,7 +18,7 @@ rm -rf /app* &>>${log_file}
 print_head "downloading app content"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
 
-cd /app
+
 
 print_head "extracting app content"
 unzip /tmp/catalogue.zip &>>${log_file}
