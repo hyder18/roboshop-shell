5795963754e38f9640848f87efa7130f6728161a
hyder18 <Shammijb123@gmail.com>
2023-06-21 17:49:49 +0530

    roboshop-component

--- frontend.sh
+++ frontend.sh
@@ -2,33 +2,29 @@ source common.sh
 
 print_head "install nginx"
 yum install nginx -y &>>${log_file}
-if [ $? -eq 0 ]; then
-  echo success
-else
-  echo failure
-fi
+status_check $?
 
 print_head "removing old content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
-echo $?
+status_check $?
 
 print_head "downloading frontend content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
-echo $?
+status_check $?
 
 print_head "extracting frontend content"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
-echo $?
+status_check $?
 
 print_head "copying nginx config for roboshop"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
-echo $?
+status_check $?
 
 print_head "enabling nginx"
 systemctl enable nginx &>>${log_file}
-echo $?
+status_check $?
 
 print_head "starting nginx"
-systemctl start nginx &>>${log_file}
-echo $?
+systemctl restart nginx &>>${log_file}
+status_check $?
