76b1cf17020e1e17624e0253deb3c71cd77d686b
hyder18 <Shammijb123@gmail.com>
2023-06-21 14:19:50 +0530

    roboshop-component

--- mongodb.sh
+++ mongodb.sh
@@ -3,15 +3,14 @@ source common.sh
 print_head "setup mongodb repository"
 cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 
-print_head "installing mongodb"
+print_head "install mongodb"
 yum install mongodb-org -y &>>${log_file}
 
-print_head "update mongodb listen adress"
+print_head "update mongodb listen address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${log_file}
 
-print_head "enabling mongodb"
-systemctl enable mongod &>>${log_file}
+print_head "enable mongodb"
+systemctl enamble mongod &>>${log_file}
 
-
-print_head "starting mongodb"
+print_head "start mongodb service"
 systemctl restart mongod &>>${log_file}
