231890cd6d61344cca3fb1c8e0dd9cc0cecc0e6d
hyder18 <Shammijb123@gmail.com>
2023-06-22 16:20:19 +0530

    roboshop-component

--- mysql.sh
+++ mysql.sh
@@ -1,13 +1,17 @@
 source common.sh
 
 mysql_root_password=$1
-if [ -z "{mysql_root_password}" ]; then
+if [ -z "${mysql_root_password}" ]; then
   echo -e "\e[31mmissing mysql_root_password argument\e[0m"
   exit 1
 fi
 
 print_head "disabling mysql 8 version"
 yum module disable mysql -y &>>${log_file}
+status_check $?
+
+print_head "copying mysql repo file"
+cp ${code_dir}/configs/mysql.repo /etc/yum.repos.d/mysql.repo &>>${log_file}
 status_check $?
 
 print_head "install mysql server"
