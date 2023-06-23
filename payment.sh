d701ae75c58cb5c467cb7b4c34ffb360ce833e46
hyder18 <Shammijb123@gmail.com>
2023-06-23 17:23:07 +0530

    roboshop-component

--- payment.sh
+++ payment.sh
@@ -1,9 +1,9 @@
 source common.sh
 
-mysql_root_password=$1
- if [ -z "${mysql_root_password}" ]; then
-     echo -e "\e[31mmissing Rabbitmq app user password  argument\e[0m"
-     exit 1
+roboshop_app_password=$1
+ if [ -z "${roboshop_app_password}" ]; then
+   echo -e "\e[31mrabbitmq app user password is argument\e[0m"
+   exit 1
  fi
 
 component=payment
