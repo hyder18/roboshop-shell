3a5228d1c4582fed94b2c888cfd3883026fe2c91
hyder18 <Shammijb123@gmail.com>
2023-06-24 13:53:39 +0530

    roboshop-component

--- dispatch.sh
+++ dispatch.sh
@@ -2,7 +2,7 @@ source common.sh
 
 roboshop_app_password=$1
  if [ -z "${roboshop_app_password}" ]; then
-   echo -e "\e[31mrabbitmq app user password is argument\e[0m"
+   echo -e "\e[31mmissing rabbitmq app user password is argument\e[0m"
    exit 1
  fi
 
